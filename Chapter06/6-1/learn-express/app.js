b0479956f12fb0503d27b3913839c50853e1fcf0
Ohhyeun Kwon <35871896+kdhgus92@users.noreply.github.com>
2025-03-19 20:50:34 +0900

    'multer'

--- Chapter06/6-1/learn-express/app.js
+++ Chapter06/6-1/learn-express/app.js
@@ -9,8 +9,8 @@ dotenv.config();
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
-// app.use(morgan("dev"));
-app.use(morgan("combined"));
+app.use(morgan("dev"));
+// app.use(morgan("combined"));
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,12 +28,69 @@ app.use(
   })
 );
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.raw());
+app.use(bodyParser.text());
+
+const multer = require("multer");
+const fs = require("fs");
+
+try {
+  fs.readdirSync("uploads");
+} catch (error) {
+  console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "multipart.html"));
+});
+
+// 파일 하나만 보낼 때.
+// app.post("/upload", upload.single("image"), (req, res) => {
+//   console.log(req.file, req.body);
+//   res.send("ok");
+// });
+
+// 파일 여러개
+// app.post("/upload", upload.array("many"), (req, res) => {
+//   console.log(req.files, req.body);
+//   res.send("ok");
+// });
+
+app.post(
+  "/upload",
+  upload.fields([{ name: "image1" }, { name: "image2" }]),
+  (req, res) => {
+    console.log(req.files, req.body);
+    res.send("ok");
+  }
+);
+
+// app.post("/uploads", upload.none(), (req, res) => {
+//   console.log(req.body);
+//   res.send("ok");
+// });
+
 app.use((req, res, next) => {
   console.log("모든 요청에 다 실행됩니다.");
   next();
 });
 
 app.get("/", (req, res, next) => {
+  req.session.id = "hello";
   res.sendFile(path.join(__dirname, "./index.html"));
   console.log("GET / 요청에서만 실행됩니다.");
 });
