593c43346f8a24ff15e2e6b45c0c8d3af1488b8e
Ohhyeun Kwon <35871896+kdhgus92@users.noreply.github.com>
2025-04-13 12:24:05 +0900

    chpater09 end

--- Chapter09/nodebird/routes/post.js
+++ Chapter09/nodebird/routes/post.js
@@ -33,13 +33,27 @@ router.post("/img", isLoggedIn, upload.single("img"), (req, res) => {
   res.json({ url: `/img/${req.file.filename}` });
 });
 
-router.post("/", isLoggedIn, upload.none(), async (req, res, next) => {
+const upload2 = multer();
+router.post("/", isLoggedIn, upload2.none(), async (req, res, next) => {
   try {
     const post = await Post.create({
       content: req.body.content,
       img: req.body.url,
       UserId: req.user.id,
     });
+
+    const hashtags = req.body.content.match(/#[^\s#]+/g);
+    if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map((tag) => {
+          return Hashtag.findOrCreate({
+            where: { title: tag.slice(1).toLowerCase() },
+          });
+        })
+      );
+      console.log(result);
+      await post.addHashtags(result.map((r) => r[0]));
+    }
 
     res.redirect("/");
   } catch (error) {
