c5e21e3d37fe25708f0a311b66a3123e802f8f68
Ohhyeun Kwon <35871896+kdhgus92@users.noreply.github.com>
2025-04-13 19:39:51 +0900

    chapter10-5 end

--- ch10/nodebird-api/routes/index.js
+++ ch10/nodebird-api/routes/index.js
@@ -20,3 +20,20 @@ router.get("/", async (req, res, next) => {
     next(err);
   }
 });
+
+router.post("/domain", isLoggedIn, async (req, res, next) => {
+  try {
+    await Domain.create({
+      UserId: req.user.id,
+      host: req.body.host,
+      type: req.body.type,
+      clientSecret: uuidv4(),
+    });
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+module.exports = router;
