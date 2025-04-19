7c0ca3bcd2082604c2ad5681cb5136581fcf77eb
Ohhyeun Kwon <35871896+kdhgus92@users.noreply.github.com>
2025-04-20 02:40:13 +0900

    chap12 complete

--- ch12/gif-chat/socket.js
+++ ch12/gif-chat/socket.js
@@ -1,5 +1,102 @@
 const SocketIO = require("socket.io");
+const axios = require("axios");
+const cookieParser = require("cookie-parser");
+const cookie = require("cookie-signature");
 
+module.exports = (server, app, sessionMiddleware) => {
+  const io = SocketIO(server, { path: "/socket.io" });
+  app.set("io", io);
+  const room = io.of("/room");
+  const chat = io.of("/chat");
+
+  // io.use((socket, next) => {
+  //   sessionMiddleware(socket.request, socket.request.res || {}, next);
+  // });
+
+  // 🔥 세션 미들웨어 Socket.IO에 적용
+  io.use((socket, next) => {
+    cookieParser(process.env.COOKIE_SECRET)(
+      socket.request,
+      socket.request.res || {},
+      () => {
+        sessionMiddleware(socket.request, {}, next);
+      }
+    );
+  });
+  chat.use((socket, next) => {
+    cookieParser(process.env.COOKIE_SECRET)(
+      socket.request,
+      socket.request.res || {},
+      () => {
+        sessionMiddleware(socket.request, {}, next);
+      }
+    );
+  });
+  room.use((socket, next) => {
+    cookieParser(process.env.COOKIE_SECRET)(
+      socket.request,
+      socket.request.res || {},
+      () => {
+        sessionMiddleware(socket.request, {}, next);
+      }
+    );
+  });
+
+  room.on("connection", (socket) => {
+    console.log("room 네임스페이스에 접속");
+    socket.on("disconnect", () => {
+      console.log("room 네임스페이스 접속 해제");
+    });
+  });
+
+  chat.on("connection", (socket) => {
+    console.log("chat 네임스페이스에 접속");
+    const req = socket.request;
+    const {
+      headers: { referer },
+    } = req;
+    const roomId = referer
+      .split("/")
+      [referer.split("/").length - 1].replace(/\?.+/, "");
+    socket.join(roomId);
+    socket.to(roomId).emit("join", {
+      user: "system",
+      chat: `${req.session.color}님이 입장하셨습니다.`,
+    });
+
+    socket.on("disconnect", () => {
+      console.log("chat 네임스페이스 접속 해제");
+      socket.leave(roomId);
+
+      const currentRoom = socket.adapter.rooms.get(roomId);
+      const userCount = currentRoom ? currentRoom.length : 0;
+      if (userCount === 0) {
+        const signedCookie = req.signedCookies["connect.sid"];
+        const connectSID = cookie.sign(signedCookie, process.env.COOKIE_SECRET);
+        // 접속자가 0명이면 방 삭제
+        axios
+          .delete(`http://localhost:8005/room/${roomId}`, {
+            headers: {
+              Cookie: `connect.sid=s%3A${connectSID}`,
+            },
+          })
+          .then(() => {
+            console.log("방 제거 요청 성공");
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      } else {
+        socket.to(roomId).emit("exit", {
+          user: "system",
+          chat: `${req.session.color}님이 퇴장하셨습니다.`,
+        });
+      }
+    });
+  });
+};
+
+/*
 module.exports = (server) => {
   const io = SocketIO(server, { path: "/socket.io" });
 
@@ -31,6 +128,7 @@ module.exports = (server) => {
     }, 3000);
   });
 };
+*/
 
 /*
 const WebSocket = require("ws");
